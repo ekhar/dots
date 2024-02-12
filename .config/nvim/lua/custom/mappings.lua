cd83649e705b75f6495061e94f8848a3ed3c9a62
Eric Khar <ericsrealemail@gmail.com>
2024-02-11 21:05:32 -0500

    nvchad

---
+++ .config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,43 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+    }
+
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+  },
+}
+
+M.copilot = {
+  i = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+      end,
+      "Copilot Accept",
+      {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+    }
+  },
+  n = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+      end,
+      "Copilot Accept",
+      {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+    }
+  }
+}
+
+-- more keybinds!
+
+return M
