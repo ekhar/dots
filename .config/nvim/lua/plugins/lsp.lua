f228ae906b4162bbd8cd3d52cc0ca78612bed3da
Eric Khar <ericsrealemail@gmail.com>
2025-01-22 00:27:57 -0500

    finally deno working

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,46 +1,18 @@
+local nvim_lsp = require("lspconfig")
+
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         denols = {
-          -- LSP workspace is scoped to Deno directories
-          root_dir = function(fname)
-            local util = require("lspconfig.util")
-            local deno_root = util.root_pattern("deno.json", "deno.jsonc")(fname)
-            if deno_root then
-              return deno_root
-            end
-            return nil
+          filetypes = { "typescript", "typescriptreact" },
+          root_dir = function(...)
+            return nvim_lsp.util.root_pattern("deno.jsonc", "deno.json")(...)
           end,
-          single_file_support = false,
-          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "json" },
-          init_options = {
-            lint = true,
-            unstable = true,
-            suggest = {
-              imports = {
-                hosts = {
-                  ["https://deno.land"] = true,
-                  ["https://cdn.nest.land"] = true,
-                  ["https://crux.land"] = true,
-                },
-              },
-            },
-          },
         },
         vtsls = {
-          -- LSP workspace scoped to non-Deno directories
-          root_dir = function(fname)
-            local util = require("lspconfig.util")
-            local deno_root = util.root_pattern("deno.json", "deno.jsonc")(fname)
-            if deno_root then
-              return nil
-            end
-            return util.root_pattern("package.json", "tsconfig.json", "jsconfig.json")(fname)
-          end,
-          single_file_support = false,
-          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
+          root_dir = nvim_lsp.util.root_pattern("package.json"),
         },
       },
     },
